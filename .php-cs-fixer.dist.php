e17f6cc11605577b44146f36ff8737447c9a588b
Rico Sonntag <mail@ricosonntag.de>
2026-06-20 15:25:31 +0200

    Add development tooling, CI and project configuration

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * This file is part of the package magicsunday/webtrees-obituary-matcher.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+/**
+ * This file represents the configuration for Code Sniffing PSR-2-related
+ * automatic checks of coding guidelines Install @fabpot's great php-cs-fixer
+ * tool via
+ *
+ *  $ composer global require friendsofphp/php-cs-fixer
+ *
+ * And then simply run
+ *
+ *  $ php-cs-fixer fix
+ *
+ * For more information read:
+ *  http://www.php-fig.org/psr/psr-2/
+ *  http://cs.sensiolabs.org
+ */
+
+if (PHP_SAPI !== 'cli') {
+    die('This script supports command line usage only. Please check your command.');
+}
+
+$header = <<<EOF
+This file is part of the package magicsunday/webtrees-obituary-matcher.
+
+For the full copyright and license information, please read the
+LICENSE file that was distributed with this source code.
+EOF;
+
+return (new PhpCsFixer\Config())
+    ->setCacheFile(__DIR__ . '/.build/cache/.php-cs-fixer.cache')
+    ->setRiskyAllowed(true)
+    ->setParallelConfig(new PhpCsFixer\Runner\Parallel\ParallelConfig(4, 8))
+    ->setRules([
+        '@PER-CS2x0'                      => true,
+        '@Symfony'                        => true,
+
+        // Additional custom rules
+        'declare_strict_types'            => true,
+        'concat_space'                    => [
+            'spacing' => 'one',
+        ],
+        'header_comment'                  => [
+            'header'       => $header,
+            'comment_type' => 'PHPDoc',
+            'location'     => 'after_open',
+            'separate'     => 'both',
+        ],
+        'method_argument_space'           => [
+            'on_multiline' => 'ensure_fully_multiline',
+        ],
+        'phpdoc_to_comment'               => false,
+        'phpdoc_no_alias_tag'             => false,
+        'phpdoc_annotation_without_dot'   => false,
+        'no_superfluous_phpdoc_tags'      => false,
+        'phpdoc_separation'               => [
+            'groups' => [
+                [
+                    'author',
+                    'license',
+                    'link',
+                ],
+            ],
+        ],
+        'no_alias_functions'              => true,
+        'whitespace_after_comma_in_array' => [
+            'ensure_single_space' => true,
+        ],
+        'single_line_throw'               => false,
+        'self_accessor'                   => false,
+        'global_namespace_import'         => [
+            'import_classes'   => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+        'function_declaration'            => [
+            'closure_function_spacing' => 'one',
+            'closure_fn_spacing'       => 'one',
+        ],
+        'binary_operator_spaces'          => [
+            'operators' => [
+                '='   => 'align_single_space_minimal',
+                '=>'  => 'align_single_space_minimal',
+                '+='  => 'align_single_space_minimal',
+                '-='  => 'align_single_space_minimal',
+                '.='  => 'align_single_space_minimal',
+                '??=' => 'align_single_space_minimal',
+            ],
+        ],
+        'yoda_style'                      => [
+            'equal'                => false,
+            'identical'            => false,
+            'less_and_greater'     => false,
+            'always_move_variable' => false,
+        ],
+        'blank_line_before_statement'     => [
+            'statements' => [
+                'break',
+                'continue',
+                'for',
+                'foreach',
+                'if',
+                'return',
+                'switch',
+                'throw',
+                'try',
+                'while',
+            ],
+        ],
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->exclude([
+                '.build',
+            ])
+            ->in([
+                __DIR__ . '/src/',
+                __DIR__ . '/tests/',
+            ])
+            ->name('*.php')
+    );
---
+++ rector.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * This file is part of the package magicsunday/webtrees-obituary-matcher.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\DeadCode\Rector\Stmt\RemoveUnreachableStatementRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src/',
+    ]);
+
+    $rectorConfig->fileExtensions(['php']);
+
+    if (
+        !is_dir($concurrentDirectory = __DIR__ . '/.build/cache/.rector.cache')
+        && !mkdir($concurrentDirectory, 0775, true)
+        && !is_dir($concurrentDirectory)
+    ) {
+        throw new \RuntimeException(
+            sprintf(
+                'Directory "%s" was not created',
+                $concurrentDirectory
+            )
+        );
+    }
+
+    if (
+        !is_dir($concurrentDirectory = __DIR__ . '/.build/cache/.rector.container.cache')
+        && !mkdir($concurrentDirectory, 0775, true)
+        && !is_dir($concurrentDirectory)
+    ) {
+        throw new \RuntimeException(
+            sprintf(
+                'Directory "%s" was not created',
+                $concurrentDirectory
+            )
+        );
+    }
+
+    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
+    $rectorConfig->importNames();
+    $rectorConfig->removeUnusedImports();
+    $rectorConfig->disableParallel();
+    $rectorConfig->cacheDirectory(__DIR__ . '/.build/cache/.rector.cache');
+    $rectorConfig->containerCacheDirectory(__DIR__ . '/.build/cache/.rector.container.cache');
+    $rectorConfig->phpVersion(80300);
+
+    // Define what rule sets will be applied
+    $rectorConfig->sets([
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::INSTANCEOF,
+        SetList::PRIVATIZATION,
+        SetList::TYPE_DECLARATION,
+        SetList::TYPE_DECLARATION_DOCBLOCKS,
+        LevelSetList::UP_TO_PHP_83,
+    ]);
+
+    // Skip some rules
+    $rectorConfig->skip([
+        CatchExceptionNameMatchingTypeRector::class,
+        RemoveUnreachableStatementRector::class,
+        RemoveUselessParamTagRector::class,
+        RemoveUselessReturnTagRector::class,
+    ]);
+};
